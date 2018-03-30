ded858258a934351a8f3ba9ec57f96314baeabda
Jean-Luke <cabinhammer@gmail.com>
2018-03-30 11:22:40 +0100

    set up init

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+var express = require('express');
 var app = express();
 
 app.get("/", function(req, resp){
 	console.log("homepage sent" )
 	resp.sendFile("/index.html")
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port,function()
+{
+	console.log("app running on port "+port);
 });
